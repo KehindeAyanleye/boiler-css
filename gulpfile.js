a5bf01cfae299f3c6e1558a91da8e9516f033683
CJ Ponti <cjponti@gmail.com>
2014-03-27 11:11:17 +0000

    Project setup

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var concat = require('gulp-concat');
+
+gulp.task('concat', function() {
+	gulp.src('build/boiler.css')
+		.pipe(concat('boiler.min.css'))
+		.pipe(gulp.dest('build'))
+});
+
+gulp.task('less', function() {
+	gulp.src('src/boiler/boiler.less')
+		.pipe(less())
+		.pipe(gulp.dest('build'))
+});
+
+gulp.task('watch', function() {
+	gulp.watch('src/boiler/**/*.less', ['less']);
+});
+
+gulp.task('default', [ 'watch' ]);
